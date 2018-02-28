e7a3a85e070b3c2ff9b012bb4adc1feac9f56062
Amr <amr.essam67@gmail.com>
2018-02-28 23:49:09 +0200

    redirect to cars listing route

--- client/src/car/create/ctrl.js
+++ client/src/car/create/ctrl.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react';
 import Form from './_form';
 import axios from 'axios'
+import { BrowserRouter as Router , Route } from 'react-router-dom'
 
 export default class CreateCarCtrl extends Component {
     state = {types: []}
     handleSubmit(data) {
       var _this= this;
         axios.post('http://localhost:8000/cars', {car: data}).then(function(response){
-
-        })
+             window.location = "/cars";
+        });
     }
     componentDidMount() {
       var _this = this;
@@ -18,7 +19,6 @@ export default class CreateCarCtrl extends Component {
       });
   }
     render() {
-      console.log("hfdsa", this)
         return (
             <div>
                 <Form onSubmit={this.handleSubmit.bind(this)} types={this.state.types}></Form>
