76749f5d82738936cf3e7ba2a2cef52899d90447
Amr <amr.essam67@gmail.com>
2018-03-01 05:24:48 +0200

    update link

--- client/src/car/list/_table.js
+++ client/src/car/list/_table.js
@@ -48,7 +48,8 @@ const Form = createClass({
                       <td>{car.name}</td>
                       <td>{car.price}</td>
                       <td>{car.horsepower}</td>
-                      <button onClick={this.deleteCar} value={car.id} type="button">DELETE</button>
+                      <td><button onClick={this.deleteCar} value={car.id} type="button">DELETE</button></td>
+                      <td><a  href={"/cars/update/"+car.id} class="active">UPDATE</a></td>
                   </tr>)
        });
       let options = this.props.types.map((type) => {
@@ -74,6 +75,7 @@ const Form = createClass({
                 <th>price</th>
                 <th>horsepower</th>
                 <th>Delete</th>
+                <th>Update</th>
               </tr>
                 {cars}
               </tbody>
